8c336c074c2d65796b90af7394302270d091775b
Veronique AUZIAS <veroniqueauzias@gmail.com>
2021-12-07 17:38:56 +0100

    configure language

--- application/config/rest.php
+++ application/config/rest.php
@@ -533,7 +533,7 @@ $config['rest_ajax_only'] = FALSE;
 | Language file to load from the language directory
 |
 */
-$config['rest_language'] = 'english';
+$config['rest_language'] = 'french';
 
 /*
 |--------------------------------------------------------------------------
